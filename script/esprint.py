607dae57d55810e1ba9cf166a1418cb448d695e5
Fytex <pedropauloqt@gmail.com>
2020-10-01 03:23:49 +0100

    Cleaner and Simpler Code

--- script/esprint.py
+++ script/esprint.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
-import sys
-import time
+from time import sleep
 
 def delay(s):
     for c in s:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.01)
+        print(c, end='', flush=True)
+        sleep(0.01)
 
 delay
 delay ("\033[48;5;0;38;5;197m Please, Enter Correct Number!")
